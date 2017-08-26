20071cf04a9c0c45161b2affbef946d80a92eee8
ratnadeep007 <ratnadeepofghato@gmail.com>
2017-08-26 16:05:43 +0530

    First commit. Complete application running

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow(){
+  win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/setup.jpg'});
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  // dev tools
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+//quit when all windows are closed
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin'){
+    app.quit();
+  }
+});
